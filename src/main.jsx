ad280069ac7d4dd154489d42e9292d330a96c4b9
mancuoj <mancuoj@gmail.com>
2024-02-18 23:08:15 +0800

    chore: init

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from "react"
+import ReactDOM from "react-dom/client"
+import "./index.css"
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <h1 className="font-serif text-4xl">Hello React</h1>
+  </React.StrictMode>,
+)
